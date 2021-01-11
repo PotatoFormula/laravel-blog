37cb34cdc92f7820332a52348e2362d291d5a017
potatoformula <potatoformula@gmail.com>
2021-01-11 13:11:25 +0800

    finish part16

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,5 +15,35 @@
         <div class="col-md-12">
             <hr>
         </div>
+    </div> <!-- end of the .row -->
+
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                        <th>Created At</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <th>{{ $post->id }}</th>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ substr($post->body, 0, 50) }}{{ strlen($post->body) > 50 ? "..." : "" }}</td>
+                            <td>{{ date('M j, Y', strtotime($post->created_at)) }}</td>
+                            <td>
+                                <a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a>
+                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
