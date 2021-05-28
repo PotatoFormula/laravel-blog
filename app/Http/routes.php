28c406ab3262327bbfc30f211056b189669e1aa5
potatoformula <potatoformula@gmail.com>
2021-05-28 10:50:14 +0800

    Add login & register pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,15 @@
 |
 */
 
+// Authentication Routes
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration Routes
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::get('/contact', 'PagesController@getContact');
 Route::get('/about', 'PagesController@getAbout');
 Route::get('/', 'PagesController@getIndex');
